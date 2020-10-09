d002bf5738a4b228dd45e8bcf25553cc1a2d6233
andrei prokofjev <andrei.prokofjev@trytrue.com>
2020-10-09 11:17:19 +0300

    add SupportMapFragment

--- app/src/main/java/com/apro/paraflight/MainActivity.kt
+++ app/src/main/java/com/apro/paraflight/MainActivity.kt
@@ -3,6 +3,12 @@ package com.apro.paraflight
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.maps.MapboxMapOptions
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.maps.SupportMapFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +17,23 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+    val options = MapboxMapOptions.createFromAttributes(this, null)
+      .camera(
+        CameraPosition.Builder()
+          .target(LatLng(59.436962, 24.753574))
+          .zoom(10.0)
+          .build()
+      )
+
+    val mapFragment = SupportMapFragment.newInstance(options).apply {
+      getMapAsync {
+        it.setStyle(Style.TRAFFIC_DAY)
+      }
+    }
+
     val ft = supportFragmentManager.beginTransaction()
-    ft.add(R.id.fragmentContainerLayout, MapboxFragment.create())
+    ft.add(R.id.fragmentContainerLayout, mapFragment, "com.mapbox.map")
     ft.commit()
   }
 }
