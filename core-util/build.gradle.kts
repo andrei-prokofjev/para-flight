952bee167e6f871380645ee8443fecf2182a0e41
andrei prokofjev <andrei.prokofjev@trytrue.com>
2020-10-12 10:28:31 +0300

    map view model

---
+++ core-util/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+  id("com.android.library")
+  id("kotlin-android")
+  id("kotlin-android-extensions")
+}
+
+
+android {
+  compileSdkVersion(Android.compileSdk)
+  buildToolsVersion(Android.buildTools)
+
+  defaultConfig {
+    minSdkVersion(Android.minSdk)
+    targetSdkVersion(Android.targetSdk)
+    versionCode = VERSION_CODE
+    versionName = VERSION_NAME
+    vectorDrawables.useSupportLibrary = true
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+}
+
+dependencies {
+
+  implementation(Libs.kotlin)
+  implementation(Libs.ktx)
+  implementation(Libs.appCompat)
+
+
+  testImplementation(TestLibs.junit)
+  androidTestImplementation(TestLibs.testJunit)
+  androidTestImplementation(TestLibs.espresso)
+
+}
