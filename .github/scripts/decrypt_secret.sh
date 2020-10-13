0c143bfc72d95aa64f66d42c4ed84f457d7fce88
andrei prokofjev <andrei.prokofjev@trytrue.com>
2020-10-13 10:33:50 +0300

    github ci

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,5 +4,5 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$MAPBOX_DOWNLOADS_TOKEN" \
---output $HOME/secrets/mapbox_token.json mapbox_token.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$MAPBOX_DOWNLOADS_TOKEN"
+#--output $HOME/secrets/mapbox_token.json mapbox_token.json.gpg
