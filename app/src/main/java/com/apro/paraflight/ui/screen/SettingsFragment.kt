f62ffa0bd8d73d41cd6227dab100750f3d9132a1
andrei prokofjev <andrei.prokofjev@trytrue.com>
2020-10-21 13:48:20 +0300

    navigation

--- app/src/main/java/com/apro/paraflight/ui/screen/SettingsFragment.kt
+++ app/src/main/java/com/apro/paraflight/ui/screen/SettingsFragment.kt
@@ -1,11 +1,35 @@
 package com.apro.paraflight.ui.screen
 
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import androidx.fragment.app.viewModels
 import com.apro.core.ui.BaseFragment
 import com.apro.paraflight.R
-import com.apro.paraflight.databinding.FragmentSettingsBinding
+import com.apro.paraflight.databinding.FragmentMainBinding
 import com.apro.paraflight.ui.base.viewBinding
+import com.apro.paraflight.viewmodel.main.MainScreenComponent
+import com.apro.paraflight.viewmodel.main.MapboxViewModel
 
-class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+class SettingsFragment : BaseFragment(R.layout.fragment_main) {
+  private val component by lazy { MainScreenComponent.create() }
+  private val binding by viewBinding { FragmentMainBinding.bind(it) }
+  private val viewModel by viewModels<MapboxViewModel> { component.viewModelFactory() }
 
-  private val binding by viewBinding { FragmentSettingsBinding.bind(it) }
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    val layout = FrameLayout(requireContext()).apply {
+
+      setBackgroundColor(Color.RED)
+
+    }
+
+    binding.mapboxLayout.addView(layout)
+
+  }
+
+
+  ond
 }
