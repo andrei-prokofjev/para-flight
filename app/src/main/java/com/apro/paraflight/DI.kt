42453b7ced7c13b96d61df87920038154babb75d
aprokofjev <aprokofjev@malwarebytes.com>
2022-12-16 15:15:25 +0200

    crowdin

--- app/src/main/java/com/apro/paraflight/DI.kt
+++ app/src/main/java/com/apro/paraflight/DI.kt
@@ -3,6 +3,7 @@ package com.apro.paraflight
 import com.apro.core.db.api.di.DatabaseApi
 import com.apro.paraflight.di.AppComponent
 
+// test
 object DI {
   lateinit var appComponent: AppComponent
 
